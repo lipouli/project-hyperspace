168737aa85a00d6d5a4f19d0ad8ec052b64c51e1
lipouli <li.lipou@gmail.com>
2018-04-01 11:57:49 +0200

    rectification

--- js/app.js
+++ js/app.js
@@ -83,10 +83,6 @@ function chooseStars(nbOfStar) {
 
 function drawStar(star) {
 
-  canvasJs.deviation.x = round(rotationX);
-  canvasJs.deviation.y = round(rotationY);
-  let div = document.getElementsByTagName('div');
-  div.textContent = rotationX + ' ' + rotationY;
   let option = {
     starZ : star.z,
     radius : star.z * star.size,
